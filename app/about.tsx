ab289851abf992da191916b4f0f61aa23857b6f3
Nguyễn Thế Hiếu <153330033+thehieu03@users.noreply.github.com>
2025-11-29 00:59:06 +0700

    screen option header true

---
+++ app/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const about = () => {
+  return (
+    <View>
+      <Text>This is the about screen</Text>
+    </View>
+  )
+}
+
+export default about
---
+++ app/index.tsx
@@ -0,0 +1,60 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.box1}>
+        <Text style={styles.text}>Box 1</Text>
+      </View>
+      <View style={styles.box2}>
+        <Text style={styles.text}>Box 2</Text>
+      </View>
+      <View style={styles.box3}>
+        <Text style={styles.text}>Box 3</Text>
+      </View>
+      <Link href="/about">Visit about screen</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+    padding: 20,
+    gap: 10,
+  },
+  box1: {
+    width: 80,
+    height: 80,
+    backgroundColor: "red",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 5,
+  },
+  box2: {
+    width: 80,
+    height: 80,
+    backgroundColor: "blue",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 5,
+  },
+  box3: {
+    width: 80,
+    height: 80,
+    backgroundColor: "green",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 5,
+  },
+  text: {
+    color: "white",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+});
